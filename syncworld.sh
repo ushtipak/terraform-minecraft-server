832dfdad544a41a9811c9c6ebade84c614b8ab7e
Milos Knezevic <ushtipak@gmail.com>
2020-03-21 17:04:44 +0100

    Add syncworld to push previously saved world state (if exists :)

---
+++ syncworld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copy world directory to Minecraft server, if `minecraft/world` exists
+
+[[ $# != 1 ]] && exit 1
+
+if [[ -d minecraft/world ]];
+	then /usr/bin/rsync -PHcuva minecraft/world root@${1}:/opt/minecraft/
+fi
+
