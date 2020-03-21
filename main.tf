58ee478c301a61dbf24a71d27313c7d4f26c1435
Milos Knezevic <ushtipak@gmail.com>
2020-03-21 20:44:24 +0100

    Upgrade to Terraform 0.12+ :)

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "digitalocean_droplet" "minecraft_server" {
-  image              = "debian-10-x64"
-  name               = "minecraft-server"
-  region             = "fra1"
-  size               = "2gb"
-  ssh_keys           = ["${var.ssh_fingerprint}"]
+  image    = "debian-10-x64"
+  name     = "minecraft-server"
+  region   = "fra1"
+  size     = "2gb"
+  ssh_keys = ["${var.ssh_fingerprint}"]
 
   connection {
     type        = "ssh"
     user        = "root"
-    private_key = "${file(var.pvt_key)}"
+    private_key = file(var.pvt_key)
     host        = self.ipv4_address
   }
 
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "minecraft-server" {
-  value = "${digitalocean_droplet.minecraft_server.ipv4_address}"
+  value = digitalocean_droplet.minecraft_server.ipv4_address
 }
 
