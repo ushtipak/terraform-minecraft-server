b3552bf8636ee5bf6147183c89936338953808af
Milos Knezevic <ushtipak@gmail.com>
2020-03-22 17:01:11 +0100

    Add terrible IP retrieval from terraform.tfstate :)

--- wrapup.sh
+++ wrapup.sh
@@ -4,9 +4,6 @@
 
 echo "wrapup start"
 
-# expect server ip as an argument
-[[ $# != 1 ]] && exit 1
-
 # if previous copy of `world` exists, copy it to `saves` with timestamp
 if [[ -d minecraft/world ]]; then
   rollback="saves/$(date +"world-"%F-%H-%M-%S-%N)"
@@ -15,6 +12,24 @@ if [[ -d minecraft/world ]]; then
   cp -r minecraft/world "${rollback}"
   echo "  copied [!]"
 fi
+
+# retrieve server ip from `terraform.tfstate`
+state="terraform.tfstate"
+if [[ ! -f "${state}" ]]; then
+  echo "missing state [${state}]; exiting ..."
+  exit 1
+fi
+output=$(cat ${state} | grep value)
+if [[ x${output} == "x" ]]; then
+  echo "unable to get ip; exiting ..."
+  exit 1
+fi
+ip=$(echo "${output}" | awk '{print $2}' | tr -d '",')
+if [[ x${ip} == "x" ]]; then
+  echo "unable to parse ip; exiting ..."
+  exit 1
+fi
+
 
 echo "all done \o/"
 
